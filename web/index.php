541b285b8015f26ba47642a7daddcafc94778f8a
icepon <ice_pon@hotmail.com>
2017-09-23 19:07:32 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@ $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+//$logger = new Logger('LineBot');
+//$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
 //$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
 //$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
