f7607b81096992013b641f993ff1a1c3f52ab228
icepon <ice_pon@hotmail.com>
2017-09-23 19:23:39 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
-//$logger = new Logger('LineBot');
+$logger = new Logger('LineBot');
 //$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
 //$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
