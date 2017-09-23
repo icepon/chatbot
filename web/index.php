059c7189582fa91cbb94588138d9ed1c8384f757
icepon <ice_pon@hotmail.com>
2017-09-23 18:40:01 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,10 +4,10 @@ require_once '../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+//$logger = new Logger('LineBot');
+//$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
